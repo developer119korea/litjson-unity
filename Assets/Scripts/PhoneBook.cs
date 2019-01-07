592de429b4f120768a976f10e2ac0d3ff3c9f902
developer119 <developer119korea@gmail.com>
2019-01-07 23:48:32 +0900

    테스트 코드 추가
    - 딕셔너리 안에 딕셔너리 까지 파싱 및 직렬화가 잘된다.

---
+++ Assets/Scripts/PhoneBook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhoneBook : JsonObject
+{
+    public Dictionary<string, Group> groups = new Dictionary<string, Group>();
+
+    public void AddGroup(Group group)
+    {
+        groups.Add(group.name, group);
+    }
+}
+
+public class Group
+{
+    public string name = string.Empty;
+    public Dictionary<string, Person> members = new Dictionary<string, Person>();
+
+    public Group()
+    {
+    }
+
+    public Group(string name)
+    {
+        this.name = name;
+    }
+
+    public void AddContact(Person contact)
+    {
+        members.Add(contact.name, contact);
+    }
+}
+
+public class Person
+{
+    public string name = String.Empty;
+    public ushort age = 0;
+    public string phoneNumber = String.Empty;
+
+    public Person()
+    {
+
+    }
+
+    public Person(string name, ushort age, string phoneNumber)
+    {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+}
+
---
+++ Assets/Scripts/SampleScene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class SampleScene : MonoBehaviour
+{
+    void Start()
+    {
+        PhoneBook pb = new PhoneBook();
+
+        Group school = new Group("MIT");
+        Group harvard = new Group("Harvard");
+
+        Person nayoung = new Person("nayoung", 21, "010-0000-000");
+        Person sohee = new Person("sohee", 22, "010-0000-000");
+        Person hyosung = new Person("hyosung", 20, "010-0000-000");
+
+        school.AddContact(nayoung);
+        school.AddContact(sohee);
+        school.AddContact(hyosung);
+
+        Person dog = new Person("dog", 21, "010-0000-000");
+        Person pig = new Person("pig", 22, "010-0000-000");
+        Person cow = new Person("cow", 20, "010-0000-000");
+
+        harvard.AddContact(dog);
+        harvard.AddContact(pig);
+        harvard.AddContact(cow);
+
+        pb.groups.Add(school.name, school);
+        pb.groups.Add(harvard.name, harvard);
+
+        string jsonData = pb.ToJson();
+        string filePath = Path.Combine(Application.streamingAssetsPath, string.Format("{0}.json", "phonebook"));
+        Debug.LogFormat("Local Client Save : {0} // {1}", filePath, jsonData);
+        File.WriteAllText(filePath, jsonData);
+
+        string readJson = File.ReadAllText(filePath);
+        PhoneBook copypb = JsonObject.Parse<PhoneBook>(readJson);
+
+        Debug.LogFormat("copy pb toJson : {0}", copypb.ToJson());
+    }
+}
